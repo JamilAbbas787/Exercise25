ba7dda8686601f303f0bd9b5efdac9c944a66a49
Jamil Abbas <jamilabbas787@gmail.com>
2018-10-24 21:43:07 -0400

    Complete

---
+++ Excercise25/Excercise25/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a sentence:  ");
+            bool userAnswer = true;
+            while (userAnswer)
+            {
+            string sentence = Console.ReadLine();       
+            string[] words = sentence.Split(' ');
+                foreach (var word in words)
+                {
+                    Console.WriteLine(word);
+                }
+             Console.Write("Would you like to continue (y/n)?:  ");
+             string continueUser = Console.ReadLine();
+             userAnswer = continueUser.Equals("y", StringComparison.OrdinalIgnoreCase);
+            }
+
+            Console.WriteLine("Goodbye!!!");
+
+
+
+      
+        }
+    }
+}
